2b1adb8bde38ee21b6fcc7c52fcf31e02e68a0da
Mykola Loyko <mykola.loyko@ffflabel.com>
2021-06-23 00:00:47 +0200

    init

---
+++ registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'MageSuite_DownloadableTermsAndConditions',
+    __DIR__
+);
